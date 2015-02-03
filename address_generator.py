c111426aa8614d9557614961ad512c629870fce1
Mike <mtdoyle@gmail.com>
2015-02-03 08:48:11 -0600

    Update address_generator.py
    Sometimes the script crashes when attempting to resolve coordinates. Adding a try/except so that the script proceeds.

--- address_generator.py
+++ address_generator.py
@@ -22,25 +22,29 @@ curr_lon = start_lon
 geocoder = Geocoder()
 
 while curr_lon < end_lon or curr_lat > end_lat:
-    results = geocoder.reverse_geocode(curr_lat, curr_lon)
-    if re.search("^(\d+)$",str(results[0]).split()[0]) is not None:
-        l = str(results[0]).split(',')
-        addr = ("%s,%s,%s"%(l[0],l[1].strip(),l[2].split()[1]))
-        print addr
-        f = open(filename,'r+a')
-        if addr not in f.read():
-            addr = addr+",%s,%s,ROOFTOP"%(curr_lat, curr_lon)
-            f.write(addr+"\n")
-        f.close()
-    if curr_lat > end_lat:
-        curr_lat = curr_lat - .002
-    else:
-        curr_lat = start_lat
-        curr_lon = curr_lon + .002
-    sleep_cycle = sleep_cycle + 1
-    if sleep_cycle == 4:
-        time.sleep(1)
-        sleep_cycle = 0
+    try:
+        results = geocoder.reverse_geocode(curr_lat, curr_lon)
+        if re.search("^(\d+)$",str(results[0]).split()[0]) is not None:
+            l = str(results[0]).split(',')
+            addr = ("%s,%s,%s"%(l[0],l[1].strip(),l[2].split()[1]))
+            print addr
+            f = open(filename,'r+a')
+            if addr not in f.read():
+                addr = addr+",%s,%s,ROOFTOP"%(curr_lat, curr_lon)
+                f.write(addr+"\n")
+            f.close()
+        if curr_lat > end_lat:
+            curr_lat = curr_lat - .002
+        else:
+            curr_lat = start_lat
+            curr_lon = curr_lon + .002
+        sleep_cycle = sleep_cycle + 1
+        if sleep_cycle == 4:
+            time.sleep(1)
+            sleep_cycle = 0
+    except:
+        continue
 
-
+if __name__ == "__main__":
+    
 
